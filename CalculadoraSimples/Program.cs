437463df24f7412e141ac511f39521e44090d164
luan-guimaraes-brasil <luan.guimaraes.brasil@gmail.com>
2026-09-17 18:29:30 -0300

    primeira alteração

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("==========CALCULADORA==========");
+            Console.WriteLine("Digite o primeiro número: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number1 + number2;
+            int subtraction = number1 - number2;
+            int multiplication = number1 * number2;
+            int division = number1 / number2;
+            int modulus = number1 % number2;
+
+            Console.WriteLine("O resultado da soma é: " + sum);
+            Console.WriteLine("O resultado da subtração é: " + subtraction);
+            Console.WriteLine("O resultado da multiplicação é: " + multiplication);
+            Console.WriteLine("O resultado da divisão é: " + division);
+            Console.WriteLine("O resultado da divisão inteira é: " + modulus);
+        }
+    }
+}
