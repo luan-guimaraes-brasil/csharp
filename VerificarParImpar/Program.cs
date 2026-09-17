32b926a92d8c7fdb3447b775119dac0c6f3c38f9
luan-guimaraes-brasil <luan.guimaraes.brasil@gmail.com>
2026-09-17 19:33:26 -0300

    primeira alteração

---
+++ VerificarParImpar/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula06
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("==========Verificação de números Pares ou Impares==========");
+            Console.WriteLine("Digite um número: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = number % 2;
+
+            if (result == 0)
+            {
+                Console.WriteLine("O número digitado é Par!");
+            }  
+            else 
+            {
+                Console.WriteLine("O número digitado é Impar!");
+            }
+        }
+    }
+}
